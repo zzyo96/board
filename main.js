a1e63b2dcee174a6135f1e7db04fc08aad7475a3
zzyo96 <40533543+zzyo96@users.noreply.github.com>
2018-10-03 14:32:24 +0800

    Update main.js

--- main.js
+++ main.js
@@ -117,7 +117,6 @@ function listenToUser(canvas){
     canvas.ontouchstart = function(aaa){
       var x = aaa.touches[0].clientX
       var y = aaa.touches[0].clientY
-      console.log(x,y)
       using = true
       if(eraserEnabled){
         context.clearRect(x-5,y-5,10,10)
